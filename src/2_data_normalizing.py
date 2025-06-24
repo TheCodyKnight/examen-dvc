1cab326ee6aa7cc3406aa200599f87d8a175f2f0
TheCodyKnight <t.lisowsky@gmx.de>
2025-06-24 12:26:19 +0200

    finalising dvc.lock

--- src/2_data_normalizing.py
+++ src/2_data_normalizing.py
@@ -2,17 +2,17 @@ import pandas as pd
 import joblib
 from sklearn.preprocessing import StandardScaler
 
-X_train = pd.read_csv('../data/processed/X_train.csv')
-X_test = pd.read_csv('../data/processed/X_test.csv')
+X_train = pd.read_csv('data/processed/X_train.csv')
+X_test = pd.read_csv('data/processed/X_test.csv')
 
-scaler = joblib.load('../models/data/scaler.pkl')
+scaler = joblib.load('models/data/scaler.pkl')
 X_train_scaled = scaler.transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
 #converting to df here because later scripts threw errors with the numpy array (commit only in part!)
 X_train_scaled = pd.DataFrame(X_train_scaled, columns=X_train.columns)
 X_test_scaled = pd.DataFrame(X_test_scaled, columns=X_test.columns)
-X_train_scaled.to_csv('../data/processed/X_train_scaled.csv', index=False)
-X_test_scaled.to_csv('../data/processed/X_test_scaled.csv', index=False)
+X_train_scaled.to_csv('data/processed/X_train_scaled.csv', index=False)
+X_test_scaled.to_csv('data/processed/X_test_scaled.csv', index=False)
 
 print("2_data_normalizing complete")
--- src/4_model_training.py
+++ src/4_model_training.py
@@ -2,13 +2,13 @@ import pandas as pd
 import joblib
 from sklearn.tree import DecisionTreeRegressor
 
-best_params = joblib.load('../models/data/best_params.pkl')
+best_params = joblib.load('models/data/best_params.pkl')
 
-X_train_scaled = pd.read_csv('../data/processed/X_train_scaled.csv')
-y_train = pd.read_csv('../data/processed/y_train.csv')
+X_train_scaled = pd.read_csv('data/processed/X_train_scaled.csv')
+y_train = pd.read_csv('data/processed/y_train.csv')
 
 model = DecisionTreeRegressor(**best_params)
 model.fit(X_train_scaled, y_train)
-joblib.dump(model, '../models/models/regressor.pkl')
+joblib.dump(model, 'models/models/regressor.pkl')
 
 print("4_model_training complete")
